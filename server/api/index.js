f061022d7f6665fcccd64d3a7abb8454560051cc
vrashabh-k <vvkam17@gmail.com>
2025-05-27 00:12:06 +0530

    deploy vercel.json 8

--- server/api/index.js
+++ server/api/index.js
@@ -8,15 +8,15 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import authRoutes from "./routes/auth.js";
-import userRoutes from "./routes/users.js";
-import postRoutes from "./routes/posts.js";
-import { register } from "./controllers/auth.js";
-import { createPost } from "./controllers/posts.js";
-import { verifyToken } from "./middleware/auth.js";
-import User from "./models/User.js";
-import Post from "./models/Post.js";
-import { users, posts } from "./data/index.js";
+import authRoutes from "../routes/auth.js";
+import userRoutes from "../routes/users.js";
+import postRoutes from "../routes/posts.js";
+import { register } from "../controllers/auth.js";
+import { createPost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+import User from "../models/User.js";
+import Post from "../models/Post.js";
+import { users, posts } from "../data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
